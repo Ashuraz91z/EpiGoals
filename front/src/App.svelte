e9fdff0a3d10c3e9a851a8ebaf125470694044f1
Lucas Fernandes <lucas.fernandes@epitech.digital>
2024-01-14 14:44:13 +0100

    ajout de quelque fichier pour le front et Commencement du MMR/EPI

---
+++ front/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+import {Router, Route } from 'svelte-routing';
+import Home from '../src/Views/HomeView/HomeView.svelte';
+
+let url = window.location.pathname;
+</script>
+
+<Router {url}>
+    <Route path="/" component={Home} />
+</Router>
