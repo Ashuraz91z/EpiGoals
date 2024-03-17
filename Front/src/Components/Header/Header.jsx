1aff1bf7042b48ce9c53120394c68076bbc15a3e
Lucas Fernandes <lucas.epitech@epitech.digital>
2024-03-17 01:10:52 +0100

    Header modifié presque finit

--- Front/src/Components/Header/Header.jsx
+++ Front/src/Components/Header/Header.jsx
@@ -22,81 +22,100 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="navbar bg-blue-700">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl" href="/">
-          EpiGoals
-        </a>
-      </div>
-      <div className="flex-none gap-8">
-        {isLogged && (
-          <div className="form-control hidden sm:flex">
-            <input
-              type="button"
-              value="Ajouter un match"
-              className="input input-bordered w-30 md:w-auto md:p-2"
-              onClick={() => {
-                window.location.href = "/add-match";
-              }}
-            />
-          </div>
-        )}
-        {isLogged ? (
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img alt="Icône de profil" src={profileIcon} />
-              </div>
+    <div className="flex-col">
+      <div className="navbar bg-blue-700">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl" href="/">
+            EpiGoals
+          </a>
+        </div>
+        <div className="flex-none gap-8">
+          {isLogged && (
+            <div className="form-control hidden sm:flex">
+              <input
+                type="button"
+                value="Ajouter un match"
+                className="input input-bordered w-30 md:w-auto md:p-2"
+                onClick={() => {
+                  window.location.href = "/add-match";
+                }}
+              />
             </div>
-            <ul
-              tabIndex={0}
-              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a className="justify-between" href="/profil">
-                  Profil
-                </a>
-              </li>
-              <li>
-                <a href="/settings">Réglages</a>
-              </li>
-              <li>
-                <a
-                  onClick={() => {
-                    Cookies.remove("token");
-                    setIsLogged(false);
-                    setProfileIcon("");
-                  }}
-                >
-                  Se déconnecter
-                </a>
-              </li>
-            </ul>
-          </div>
-        ) : (
-          <div className="flex gap-2">
-            <input
-              type="button"
-              value="Login"
-              className="btn"
-              onClick={() => {
-                window.location.href = "/login";
-              }}
-            />
-            <input
-              type="button"
-              value="Register"
-              className="btn"
-              onClick={() => {
-                window.location.href = "/register";
-              }}
-            />
-          </div>
-        )}
+          )}
+          {isLogged ? (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img alt="Icône de profil" src={profileIcon} />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between" href="/profil">
+                    Profil
+                  </a>
+                </li>
+                <li>
+                  <a href="/settings">Réglages</a>
+                </li>
+                <li>
+                  <a
+                    onClick={() => {
+                      Cookies.remove("token");
+                      setIsLogged(false);
+                      setProfileIcon("");
+                    }}
+                  >
+                    Se déconnecter
+                  </a>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div className="flex gap-2">
+              <input
+                type="button"
+                value="Login"
+                className="btn"
+                onClick={() => {
+                  window.location.href = "/login";
+                }}
+              />
+              <input
+                type="button"
+                value="Register"
+                className="btn"
+                onClick={() => {
+                  window.location.href = "/register";
+                }}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="p-2">
+        <ul className="flex gap-6 p-2 bg-white justify-center m-auto rounded-md h-12 items-center w-max">
+          <li>
+            <a href="/match" className="hover:drop-shadow hover:text-black">
+              Match
+            </a>
+          </li>
+          <li className="hover:drop-shadow hover:text-black ">
+            <a href="/notifications">Notifications</a>
+          </li>
+          <li>
+            <a href="/test" className="hover:text-black hover:drop-shadow">
+              Carrière
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
